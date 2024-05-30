4c298973e0c9a0251de4daae0f3836565fc5be05
Marco Portacio <marcoportacio4@gmail.com>
2024-05-30 00:10:46 -0500

    Removed email and password default values

--- app/src/main/java/com/markusw/timetonictest/auth/presentation/LoginState.kt
+++ app/src/main/java/com/markusw/timetonictest/auth/presentation/LoginState.kt
@@ -3,8 +3,8 @@ package com.markusw.timetonictest.auth.presentation
 import com.markusw.timetonictest.core.presentation.UiText
 
 data class LoginState(
-    val email: String = "android.developer@timetonic.com",
-    val password: String = "Android.developer1",
+    val email: String = "",
+    val password: String = "",
     val isLoading: Boolean = false,
     val emailError: UiText? = null,
     val passwordError: UiText? = null,
